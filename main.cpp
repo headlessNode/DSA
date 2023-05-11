a06989b60a92a84a7a0812d0f548bb124a1e6188
Muhammad Haris <101793258+Gr1m412@users.noreply.github.com>
2023-05-12 01:09:07 +0500

    Update main.cpp
    - Added comments following coding conventions.
    - Added a line of code to de-allocate memory, on the heap.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 
-int* passing_array(int size);
+int* passing_array(int size); // Function declaration for passing_array
 
 
 int main()
 {
-	int* ptr_array{};
+	int* ptr_array{}; // Declare a pointer variable and initialize it to nullptr
 
-	ptr_array = passing_array(5);
+	ptr_array = passing_array(5); // Call the passing_array function and assign the returned pointer to ptr_array
 
-	//
-	*ptr_array = 10;
-	*(ptr_array + 1) = 20;
-	//
-	std::cout << *ptr_array << " " << *(ptr_array + 1) << "\n";
-
+	
+	*ptr_array = 10; // Assign the value 10 to the first element of the dynamically allocated array
+	*(ptr_array + 1) = 20; // Assign the value 20 to the second element of the dynamically allocated array using pointer arithmetic
+	
+	std::cout << *ptr_array << " " << *(ptr_array + 1) << "\n"; // Print the values of the first and second elements of the array
+	
+	delete [] ptr_array; // deallocate the memory
+	
 	return 0;
 }
 
 int* passing_array(int size) 
 {
-	int* ptr = new int[size];
-	return ptr;
+	int* ptr = new int[size]; // Dynamically allocate an array of integers with the specified size
+	return ptr; // Return the pointer to the dynamically allocated array
 }
