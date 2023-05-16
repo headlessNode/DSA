00f301f3e48e1f3e057f8d389c09240eacd91399
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2023-05-16 22:04:41 +0500

    Update factorial_recursion.cpp
    - added iterative method of factorial.
    - added comments

--- recursion/factorial_recursion.cpp
+++ recursion/factorial_recursion.cpp
@@ -3,8 +3,18 @@
 /*
 A factorial is a function that multiplies a number by every number below it till 1. 
 For example, the factorial of 3 represents the multiplication of numbers 
-3, 2, 1, i.e. 3! = 3 × 2 × 1 and is equal to 6.
+3, 2, 1, i.e. 3! = 3 Ã— 2 Ã— 1 and is equal to 6.
 */
+int fact(int n);/*Prototype of function which uses recursion to calculate factorial*/
+
+int fact1(int n); /*Prototype of function fact1 which uses iterative method to calculate factorial*/
+
+int main() {
+	int n = 3;
+	int f = fact(n);
+	std::cout << f << "\n";
+	return 0;
+}
 
 int fact(int n) {
 	if (n == 0){
@@ -15,13 +25,11 @@ int fact(int n) {
 	}
 }
 
-int main() {
-	int n = 3;
-	int f = fact(n);
-	std::cout << f << "\n";
-	return 0;
+int fact1(int n)
+{
+	int x = 1;
+	for(int i = 0; i<n ; i++){
+		x = x * i;
+	}
+	return x;
 }
-
-
-
-
