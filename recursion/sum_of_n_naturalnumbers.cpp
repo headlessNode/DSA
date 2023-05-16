1e17403dfd886740501d83830601d932d0a0ad4e
Muhammad Haris <101793258+headlessNode@users.noreply.github.com>
2023-05-16 18:16:19 +0500

    Update sum_of_n_naturalnumbers.cpp
    - Added code for the sum of "n" natural numbers using a while loop.
    
    - Added code for the sum of "n" natural numbers using (n * (n+1))/2 formula.
    
    - Added Time and Space complexities for each code snippet.

--- recursion/sum_of_n_naturalnumbers.cpp
+++ recursion/sum_of_n_naturalnumbers.cpp
@@ -1,24 +1,68 @@
 /*
 Following program prints the sum of
 "n" consective natural numbers using
-recursion
+recursion. Time complexity O(n).
+Space complexity O(n).
 */
 
 #include<iostream>
 
-int func1(int n);
+int sum(int n);
 
 int main() {
 	int n = 5;
-	std::cout << func1(n) << "\n";
+	std::cout << sum(n) << "\n";
 
 	return 0;
 }
 
-int func1(int n) {
+int sum(int n) {
 
-	if (n > 0) {
-		return func1(n - 1) + n;
+	if (n == 0) {
+		return 0;
 	}
-	return 0;
+	else{
+		return sum(n-1) + n;
+	}
 }
+
+/*
+Following code prints sum of "n" natural
+numebers using while loop. Time complexity
+O(n/2) = O(n). Space Complexity O(1).
+
+#include<iostream>
+
+
+int main() {
+
+	int n = 10;
+
+	int sum{};
+
+	while (n > 0) {
+		sum += (n - 1) + n;
+		n = n - 2;
+	}
+	std::cout << sum;
+
+}
+*/
+
+/*
+Following code uses formula (n(n+1))/2
+to calculate sum of "n" natural numbers.
+Time complexity O(1). Space complexity O(1).
+
+#include<iostream>
+
+
+int main() {
+
+	int n = 10;
+	int sum = (n * (n+1))/2
+	std::cout << sum;
+
+}
+
+*/
